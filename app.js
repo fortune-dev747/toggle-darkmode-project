6badf7a6e8f2b73d73ea2ba686c83099863e622e
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-19 18:34:59 +0100

    updated JS

--- app.js
+++ app.js
@@ -15,20 +15,3 @@ toggle.addEventListener("click", function() {
         body.style.transition = "2s";
     }
 });
-
-const toggle = document.getElementById('toggleDark');
-const body = document.querySelector('body');
-
-toggle.addEventListener('click', function() {
-    this.classList.toggle('bi-moon-fill');
-    if (this.classList.toggle('bi-brightness-high-fill')) {
-        body.style.background = '#fff';
-        body.style.color = '';
-        body.style.transition = '1s ease';
-    }
-    else {
-        body.style.background = 'rgb(17, 24, 39)';
-        body.style.color = 'text-white';
-        body.style.transition = '2s ease';
-    }
-});
